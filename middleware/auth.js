49ae7a50d1c9461dda92b5bf44f80b05238b683f
tushar756 <112198307+tushar756@users.noreply.github.com>
2024-02-23 23:42:21 +0530

    add check at the token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 const protect = (req, res, next) => {
   try {
- 
+    if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
+      throw new Error("Authorization token missing or invalid");
+    }
     const token = req.headers.authorization.split(" ")[1];
    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
