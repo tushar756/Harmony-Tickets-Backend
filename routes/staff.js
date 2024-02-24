fe588d3c1e79ef7bf81f981eabff7de04c95100c
tushar756 <112198307+tushar756@users.noreply.github.com>
2024-02-24 16:20:34 +0530

    check 1

--- routes/staff.js
+++ routes/staff.js
@@ -4,14 +4,14 @@ const staffRouter = express.Router();
 
 // all ticket assoicated with the staff
 
-const { staffTicket, counters, createReport, getAllReport, staffOpenTickets, staffResolveTickets, staffHighPriorityTickets, staffMidPriorityTickets, staffLowPriorityTickets } = require("../controller/staff.js");
+const { staffTicket, counters, createReport, getAllReport, staffOpenTickets, staffResolveTickets, staffHighPriorityTickets, staffMidPriorityTickets, staffLowPriorityTickets, staffPendingTickets } = require("../controller/staff.js");
 
 staffRouter.get("/staffTickets", staffTicket);
 staffRouter.post("/createReport", createReport);
 staffRouter.get("/getAllReport", getAllReport);
 staffRouter.get("/counts", counters);
 staffRouter.get("/staffOpenTickets", staffOpenTickets);
-staffRouter.get("/staffPendingTickets", staffOpenTickets);
+staffRouter.get("/staffPendingTickets", staffPendingTickets);
 staffRouter.get("/staffResolveTickets", staffResolveTickets);
 staffRouter.get("/staffHighPriorityTickets", staffHighPriorityTickets);
 staffRouter.get("/staffMidPriorityTickets", staffMidPriorityTickets);
